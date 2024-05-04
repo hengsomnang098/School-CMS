71a73a1d3f903e9e5a23116837a7cfc337be4398
heng somnang <hsomnang098@gmail.com>
2024-05-04 18:26:57 +0700

    antd

--- client/admin-dashboard/src/App.jsx
+++ client/admin-dashboard/src/App.jsx
@@ -1,7 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./page/HomePage";
+import AboutPage from "./page/AboutPage";
+import ContactPage from "./page/ContactPage";
+import NotFoundPage from "./page/NotFoundPage";
+import MainLayout from "./layout/MainLayout";
+
 function App() {
   return (
     <>
-      <div className="bg-red-500"> Homepage</div>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
