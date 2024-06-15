8dc37ee47895a0a8002d8b8ed8ab388040e32e60
Ly Lihieng <heangly491@gmail.com>
2024-06-15 10:18:51 +0700

    hello

--- client/cms/src/app/api/config/api.js
+++ client/cms/src/app/api/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8080/api/";
-// const BASE_URL = "http://194.233.87.193:8080/api/";
+// const BASE_URL = "http://localhost:8080/api/";
+const BASE_URL = "http://194.233.87.193:8080/api/";
 export const fetchData = async (endpoint) => {
   try {
     const response = await axios.get(`${BASE_URL}${endpoint}`);
