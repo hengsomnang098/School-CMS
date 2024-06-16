b57834c9c9cd7d89d8b48f9a364b648f8afe5386
Ly Lihieng <heangly491@gmail.com>
2024-06-16 19:52:13 +0700

    hello

--- client/cms/src/app/features/homepage/components/Events/ContentCardEventHome.jsx
+++ client/cms/src/app/features/homepage/components/Events/ContentCardEventHome.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 
 const ContentCardE = ({ content }) => {
-  const { title, description, imageUrl, mediaList } = content;
+  const title = content?.title || "";
+  const description = content?.description || "";
+  const imageUrl = content?.imageUrl || "";
 
   const maxLengthTitle = 60;
   const maxLengthDesc = 100;
+
   const truncatedDescription =
     description.length > maxLengthDesc
       ? description.substring(0, maxLengthDesc) + "..."
@@ -12,24 +15,24 @@ const ContentCardE = ({ content }) => {
 
   const truncatedTitle =
     title.length > maxLengthTitle
-      ? description.substring(0, maxLengthTitle) + "..."
+      ? title.substring(0, maxLengthTitle) + "..."
       : title;
+
   return (
     <div className="border rounded-sm bg-gray-200 w-[350px] h-[470px]">
       <div>
-        <img className="h-[250px] w-[100%] rounded-sm" src={imageUrl} alt="" />
+        <img
+          className="h-[250px] w-full rounded-t-sm object-cover"
+          src={imageUrl}
+          alt={title}
+        />
       </div>
-      <p className="text-start font-khmer font-bold text-lg mb-2 p-2">
-        {truncatedTitle}
-      </p>
-
-      <div className="prose font-khmer text-start p-2 h-[85px]">
+      <p className="text-start font-bold text-lg mb-2 p-2">{truncatedTitle}</p>
+      <div className="prose text-start p-2 h-[85px]">
         {truncatedDescription}
       </div>
-      <div className="relative h-12 w-auto">
-        <button className="absolute h-10 w-22 bottom-0 end-0 mb-4 mr-5 bg-green-400 rounded-sm px-4 text-center">
-          See More
-        </button>
+      <div className="flex justify-end p-2">
+        <button className="bg-green-400 rounded-sm px-4 py-2">See More</button>
       </div>
     </div>
   );
