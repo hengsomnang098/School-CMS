e16fe4aa3695347ecdb2d9139c5e2176409860da
Ly Lihieng <heangly491@gmail.com>
2024-07-07 15:37:15 +0700

    react

--- client/cms/src/features/homepage/components/Contents/SingleContent.jsx
+++ client/cms/src/features/homepage/components/Contents/SingleContent.jsx
@@ -7,7 +7,7 @@ const SingleContent = () => {
   const { id } = useParams();
 
   const [content, setContent] = useState({});
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true); // State to track loading status
 
   useEffect(() => {
     const fetchContent = async () => {
@@ -15,10 +15,10 @@ const SingleContent = () => {
         const contentsRes = await fetchData(`contents/${id}`);
         setContent(contentsRes.object);
 
-        setLoading(false);
+        setLoading(false); // Set loading to false when data is fetched
       } catch (error) {
         console.error("Error fetching content:", error);
-        setLoading(false);
+        setLoading(false); // Set loading to false on error as well
       }
     };
 
