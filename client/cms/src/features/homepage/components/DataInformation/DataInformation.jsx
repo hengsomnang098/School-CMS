927180e4208b095e2792f2c221130feedc3d7091
Ly Lihieng <heangly491@gmail.com>
2024-07-09 20:31:22 +0700

    react eafsf

--- client/cms/src/features/homepage/components/DataInformation/DataInformation.jsx
+++ client/cms/src/features/homepage/components/DataInformation/DataInformation.jsx
@@ -54,8 +54,8 @@ const DataInformation = () => {
 
   const renderCard = (icon, name, description) => (
     <div className="mb-2 md:w-1/2 lg:w-1/4 p-2 text-center items-center justify-center flex-col flex">
-      <div className="mb-2 flex flex-col md:flex-row   items-center">
-        <div className="h-10 flex justify-center  md:mr-4">{icon}</div>
+      <div className="mb-2 flex flex-col md:flex-row md:ml-8  items-center">
+        <div className="h-10 flex justify-center  md:mr-2">{icon}</div>
         <span className="font-mono tracking-wider text-center justify-center items-center text-2xl font-bold text-white md:text-2xl lg:text-3xl">
           {name}
         </span>
